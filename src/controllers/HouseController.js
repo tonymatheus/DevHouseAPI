47f3dc954eb9bccd5810d43f9f70c6723cb44394
tonymatheus <tony.chaves14@gmail.com>
2021-04-12 05:53:31 -0300

    Cria controller de houses passando dados que  serão retornados ao cadastro de uma nova casa

---
+++ src/controllers/HouseController.js
@@ -0,0 +1,22 @@
+import House from "../models/House";
+
+class Housecontroller {
+  async store(req, res) {
+    const { filename } = req.file;
+    const { description, price, location, status } = req.body;
+    const { user_id } = req.headers;
+
+    const house = await House.create({
+      user: user_id,
+      thumbnail: filename,
+      description,
+      price,
+      location,
+      status,
+    });
+
+    return res.json(house);
+  }
+}
+
+export default new Housecontroller();
