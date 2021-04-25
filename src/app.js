a225b40eddba4470a7d587431281a216a473a156
tonymatheus <tony.chaves14@gmail.com>
2021-04-25 14:05:47 -0300

    Adiciona CORS aos projeto da  api

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 import path from "path";
 import routes from "./routes";
 
@@ -17,6 +18,7 @@ class App {
   }
 
   middlewares() {
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
       "/files",
