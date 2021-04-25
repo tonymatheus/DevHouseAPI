696058a62d244680562bf91ec5de010529cb5f62
tonysousa <tony.sousa@meta.com.br>
2021-04-25 03:11:41 -0300

    Adiciona rota para  atualização de casa

--- src/controllers/HouseController.js
+++ src/controllers/HouseController.js
@@ -1,6 +1,45 @@
 import House from "../models/House";
-
+import User from "../models/User";
 class Housecontroller {
+  async index(req, res) {
+    const { status } = req.query;
+
+    const houses = await House.find({ status });
+
+    return res.json(houses);
+  }
+
+  async update(req, res) {
+    const { filename } = req.file;
+    const { house_id } = req.params;
+    const { description, price, location, status } = req.body;
+    const { user_id } = req.headers;
+
+    const user = await User.findById(user_id);
+    const houses = await House.findById(house_id);
+
+    if (String(user._id) !== String(houses.user)) {
+      return res
+        .status(401)
+        .json({ erro: "Usuário não autorizado para atualizar casa" });
+    }
+
+    await House.updateOne(
+      { _id: house_id },
+      {
+        user: user_id,
+        thumbnail: filename,
+        description,
+        price,
+        location,
+        status,
+      }
+    );
+
+    return res.send();
+  }
+
+  //cadastra novas casas
   async store(req, res) {
     const { filename } = req.file;
     const { description, price, location, status } = req.body;
